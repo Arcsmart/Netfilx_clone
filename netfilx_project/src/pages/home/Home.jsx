4a2e0de2b83faa9b44c7a833971131834a8bb47d
Tesfaye <tesfayealemayehu796@gmail.com>
2025-01-24 11:55:52 +0300

    heder and footer

---
+++ netfilx_project/src/pages/home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from '../../Commponent/Header/Header'
+import Footer from '../../Commponent/Footer/Footer'
+
+const Home = () => {
+  return (
+   <>
+   <Header/>
+   <Footer/>
+   </>
+   
+  )
+}
+
+export default Home
